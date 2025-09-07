097ed0c9c57fcf8f318b926abc510de25b6f2aeb
Aaradhya Gupta <aaradhyagupta0001@gmail.com>
2025-09-07 19:36:27 +0000

    our main aap is ready deand some files are deleted. now we r going to make it super duper playground for gen z. coming soon...

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// public folder serve करना
 app.use(express.static(path.join(__dirname, 'public')));
+
+// सभी GET requests के लिए index.html serve करना
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`LevelUp running at http://localhost:${PORT}`);
